590e6bd0e327ac1ea14b961bf16ffa3543a631b6
RC <rainingchain@gmail.com>
2025-06-26 20:09:55 -0400

    shuffle complete

--- PkCompletionist.Core/Gen7b/SAV7_Shuffle.cs
+++ PkCompletionist.Core/Gen7b/SAV7_Shuffle.cs
@@ -3,8 +3,25 @@ using System.Collections.Generic;
 using System;
 using System.Buffers.Binary;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Reflection;
 
 namespace PkCompletionist.Core;
+
+enum Enhancement
+{
+    MegaSpeedup,
+    RaiseMaxLevel,
+    LevelUp,
+    ExpBoosterS,
+    ExpBoosterM,
+    ExpBoosterL,
+    SkillBoosterS,
+    SkillBoosterM,
+    SkillBoosterL,
+    SkillSwapper,
+
+}
+
 partial class SAV7_Shuffle : SAV_Dummy
 {
     public SAV7_Shuffle(byte[] data) : base(data)
@@ -37,8 +54,8 @@ partial class SAV7_Shuffle : SAV_Dummy
 
     public bool HasMon(int id)
     {
-        var bit_index = IdToIndex(id) % 8;
-        var byte_offset = IdToIndex(id) / 8;
+        var bit_index = MonIdToIndex(id) % 8;
+        var byte_offset = MonIdToIndex(id) / 8;
 
         foreach (int offset in new[] { 0xE6, 0x546, 0x5E6 })
         {
@@ -48,16 +65,49 @@ partial class SAV7_Shuffle : SAV_Dummy
         return true;
     }
 
+    static List<int> MISSION_COUNT_BY_CARD = new List<int>{ 
+        3, 8, 8, 5, 5, 7, 5, 7, 7, 5, 5, 6, 7, 3, 5, 7, 3, 4, 4
+    };
+
+
+    public bool HasCompletedMissionCard(int card)
+    {
+
+        for (int missionIdx = 0; missionIdx < MISSION_COUNT_BY_CARD[card]; missionIdx++)
+        {
+            var byte_offset = 0xB6FC + CardIdToIndex(card, missionIdx) / 8;
+            var bit_index = CardIdToIndex(card, missionIdx) % 8;
+            if (((this.Data[byte_offset] >> bit_index) & 1) == 0)
+                return false;
+        }
+        return true;
+    }
+    
     public bool SetCaughtMon(int id)
     {
-        var bit_index = IdToIndex(id) % 8;
-        var byte_offset = IdToIndex(id) / 8;
+        var bit_index = MonIdToIndex(id) % 8;
+        var byte_offset = MonIdToIndex(id) / 8;
 
         foreach (int offset in new[] { 0xE6, 0x546, 0x5E6 })
         {
             SetBit(offset + byte_offset, bit_index);
         }
         return true;
+    }
+
+    public int GetEnhancementCount(Enhancement id)
+    {
+        return (Data[0x2D4C + (int)id] >> 1) & 0x7F;
+    }
+
+    public bool HasMegaStone(int monId, int stoneIdx = 0)
+    {
+        var byte_index = 0x406 + (monId + 2) / 4;
+        var bit_index = (5 + (monId * 2)) % 8;
+
+        var owned_stones = (Data[byte_index] >> bit_index);
+        var wanted_stone_mask = stoneIdx == 0 ? 0b01 : 0b10;
+        return (owned_stones & wanted_stone_mask) != 0;
     }
 
     private bool GetBit(int byte_offset, int bit_index)
@@ -69,8 +119,12 @@ partial class SAV7_Shuffle : SAV_Dummy
         Data[byte_offset] |= (byte)(1 << bit_index);
     }
 
-    private int IdToIndex(int id)
+    private int MonIdToIndex(int id)
     {
         return id + 5;
+    }
+    private int CardIdToIndex(int id, int mission)
+    {
+        return id * 10 + mission;
     }
 }
